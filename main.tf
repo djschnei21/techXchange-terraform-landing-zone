28424e1c409d4a87bfb639344c18e555f3517ccd
Dan Schneider <32646250+djschnei21@users.noreply.github.com>
2025-10-06 14:28:03 -0400

    module version first try

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  
+}
+
+module "terraform-landing-zone" {
+  source  = "app.terraform.io/djs-tfcb/terraform-landing-zone/tfe"
+  version = "1.0.2"
+  tf_org = "djs-tfcb"
+  gh_org = "djschnei21"
+  oauth_client_name = "github-test"
+  vcs = true
+  tfe_project = true
+  app_ids = [
+    "AAA",
+    "BBB"
+  ]
+  app_envs = [ "development", "test", "production" ]
+}
